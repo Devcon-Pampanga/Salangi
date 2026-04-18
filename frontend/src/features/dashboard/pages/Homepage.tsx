e7907d18cad4cb20cf975ac19a9a21cb4c69723c
kyeban19 <kyeban19@gmail.com>
2026-04-19 00:27:08 +0800

    fix: add feedback form in the mobile view

--- frontend/src/features/dashboard/pages/Homepage.tsx
+++ frontend/src/features/dashboard/pages/Homepage.tsx
@@ -235,6 +235,17 @@ function Homepage() {
               >
                 List Your Business
               </button>
+              <a href="https://forms.gle/h396M5kqkVXtQYhr5"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="flex items-center justify-center gap-3 px-4 py-3.5 bg-[#2D2D2D] text-[#FBFAF8]/60 hover:text-[#FFE2A0] border border-[#373737] rounded-xl font-semibold text-md w-full shadow-lg active:scale-95 transition-all"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 text-[#FFE2A0]">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                </svg>
+                Feedback
+              </a>
               <button
                 onClick={() => { setIsMobileMenuOpen(false); setIsSettingsOpen(true); }}
                 className="flex items-center justify-center gap-3 px-4 py-3.5 bg-[#373737] text-[#FBFAF8] rounded-xl font-semibold text-md w-full shadow-lg active:scale-95 transition-all cursor-pointer"
